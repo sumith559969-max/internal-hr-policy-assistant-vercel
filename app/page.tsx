747e855b52d401931157c27c94d34a0030cb7d49
sumith559969-max <sumith559969@gmail.com>
2026-09-18 04:25:41 +0530

    Refactor script injection in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,23 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const injectScript = document.createElement("script");
-    injectScript.src = "https://cdn.botpress.cloud/webchat/v5.0/inject.js";
-    injectScript.async = true;
+    const script1 = document.createElement("script");
+    script1.src = "https://cdn.botpress.cloud/webchat/v5.0/inject.js";
+    script1.async = true;
 
-    injectScript.onload = () => {
-      const configScript = document.createElement("script");
-      configScript.src =
+    script1.onload = () => {
+      const script2 = document.createElement("script");
+      script2.src =
         "https://files.bpcontent.cloud/2026/09/15/12/20260915121031-K9U9VQIN.js";
-      configScript.defer = true;
+      script2.defer = true;
 
-      document.body.appendChild(configScript);
+      document.body.appendChild(script2);
     };
 
-    document.body.appendChild(injectScript);
+    document.body.appendChild(script1);
 
     return () => {
-      injectScript.remove();
+      script1.remove();
     };
   }, []);
 
@@ -37,6 +37,7 @@ export default function Home() {
       <div
         id="bp-embedded-webchat"
         style={{
+          position: "relative",
           width: "100%",
           height: "100vh",
         }}
